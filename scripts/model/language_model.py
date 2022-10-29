fix pad masking errors

scripts/model/language_model.py
@@ -32,6 +32,7 @@ class TransformerLM(nn.Module):
         self.r = config.radius
         self.word_embeddings = nn.Parameter(torch.Tensor(config.vocab_size, config.embedding_dims))
         self.position_embeddings = None
+        self.pos_padding_idx = config.max_length
         self.decoder = TransformerEncoder(config=config)
         self.linear = None
         if not config.tied_embeddings:
@@ -41,8 +42,8 @@ class TransformerLM(nn.Module):
             self.out_bias = nn.Parameter(torch.Tensor(config.vocab_size))
             torch.nn.init.zeros_(self.out_bias)
         if config.add_positions:
-            self.position_embeddings = nn.Parameter(torch.Tensor(config.max_length, config.embedding_dims))
-        self.pad_idx: int = config.pad_idx
+            self.position_embeddings = nn.Parameter(torch.Tensor(config.max_length + 1, config.embedding_dims))
+        self.word_padding_idx: int = config.pad_idx
         self.embedding_dims = config.embedding_dims
         # initializing word embeddings
         if self.fix_norm:
@@ -65,7 +66,7 @@ class TransformerLM(nn.Module):
             torch.nn.init.zeros_(module.bias)
             torch.nn.init.ones_(module.weight)
     
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor, apply_mask=True) -> Tensor:
         """
         Forward across all the transformer decoder layers.
 
@@ -75,24 +76,26 @@ class TransformerLM(nn.Module):
             The input sequences. Must be of shape [b, s] where 'b'\
             is the batch size, 's' the length of the sequences in the batch.
         """
-        _, s = x.shape
+        b, s = x.shape
         device = x.device
-
-        # masking future
-        mask = torch.ones(s, s).triu(1).bool().to(device)
-        # masking pad indexes
-        pad_mask = (x == self.pad_idx)[-1, ...]
-        mask[:, pad_mask] = True
-
         w_embeddings = (self.r * F.normalize(self.word_embeddings, dim=-1)
                         if self.fix_norm else self.word_embeddings)
-        e = F.embedding(input=x, weight=w_embeddings, padding_idx=self.pad_idx)
-
+        e = F.embedding(input=x, weight=w_embeddings, padding_idx=self.word_padding_idx)
+        mask = None
+        # masking pad indexes
+        if apply_mask:
+            pad_mask = (x == self.word_padding_idx).to(device)
         if self.position_embeddings is not None:
-            positions = torch.arange(0, s, dtype=torch.long, device=device).unsqueeze(0)
-            p_embeddings = F.embedding(input=positions, weight=self.position_embeddings, padding_idx=self.pad_idx)
+            positions = torch.arange(0, s).unsqueeze(0).repeat(b, 1).to(device)
+            if apply_mask:
+                positions[pad_mask] = self.pos_padding_idx
+            p_embeddings = F.embedding(input=positions, weight=self.position_embeddings, padding_idx=self.pos_padding_idx)
             e += p_embeddings
-
+        if apply_mask:
+            pad_mask = pad_mask.unsqueeze(1).repeat(1, s, 1)
+            # masking future
+            mask = torch.ones(s, s).triu(1).unsqueeze(0).repeat(b, 1, 1).bool().to(device)
+            mask[pad_mask] = True
         c = self.decoder(e, mask)
         if self.linear is None:
             # tied embeddings

scripts/model/transformer_layer.py
@@ -86,9 +86,9 @@ class MultiHeadAttention(nn.Module):
 
         # dot-product and use the scaling factor from 'Attention is all you need" paper
         # (https://arxiv.org/pdf/1706.03762.pdf)
-        QK = (Q @ K) / 1 / torch.sqrt(torch.tensor(K.shape[-1])) # shape=[b, h, s_q, s_k]
+        QK = (Q @ K) / torch.sqrt(torch.tensor(K.shape[-1])) # shape=[b, h, s_q, s_k]
         if mask is not None:
-            QK = QK.masked_fill(mask, float('-inf'))
+            QK = QK.masked_fill(mask.unsqueeze(1).repeat(1, self.heads, 1, 1), float('-inf'))
         attention = self.softmax(QK) # shape=[b, h, s, s])
         # for each word, concatenate the attention vectors comming from all the heads.
         out = (attention @ V).view(b, s_q, -1) # [b, s, embedd_dims]
@@ -110,7 +110,7 @@ class TransformerLayer(nn.Module):
         self.layer_norm1 = nn.LayerNorm(normalized_shape=config.embedding_dims)
         self.mlp = nn.Sequential(
             nn.Linear(in_features=config.embedding_dims, out_features=config.ff_size),
-            nn.GELU(),
+            nn.ReLU(),
             nn.Linear(in_features=config.ff_size, out_features=config.embedding_dims)
         )
 

scripts/trainer.py
@@ -53,7 +53,7 @@ def get_optimizer(model):
     return optim_groups
 def train(model, traingenerator, validgenerator, device, output_path, config) :
     """Train the language model and print progression."""
-    pad_idx = model.pad_idx
+    pad_idx = model.word_padding_idx
     optim_groups = get_optimizer(model)
     cross_entropy = nn.CrossEntropyLoss(reduction="mean", ignore_index=pad_idx)
     optimizer = torch.optim.AdamW(optim_groups, lr=config.lr)
@@ -85,7 +85,8 @@ def train(model, traingenerator, validgenerator, device, output_path, config) :
                 loss.backward()
                 # if number of gradients accumulations reached then update the parameters
                 accumulations = 0
-                nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) # if the norm of the gradients vector is superior to 5, then the gradient is so to 5.
+                if config.norm_clip is not None:
+                    nn.utils.clip_grad_norm_(model.parameters(), max_norm=config.norm_clip) # if the norm of the gradients vector is superior to 5, then the gradient is so to 5.
                 optimizer.step() # update parameters
                 # scheduler taking account only the number of time the parameters are update ... 
                 # that is the accumulation iterations and not just the number of batchs.
@@ -128,7 +129,6 @@ def main():
     model = TransformerLM(config)
     get_optimizer(model)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    assert device.type == "cuda", """Cannot train on CPU"""
     LOGGER.info(f"Using device {device}")
     model.to(device)
     if config.checkpoint is not None: